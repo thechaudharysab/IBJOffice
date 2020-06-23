844881812741d3a4c909c471827b7af68a1641fa
Chaudhry Talha <gameral@live.com>
2020-06-23 17:41:04 +0800

    back to heroku

--- Program.cs
+++ Program.cs
@@ -16,19 +16,19 @@ namespace IBJOffice
         public static void Main(string[] args)
         {
             //This setting below isused by part 2 of the tutorial
-            //var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            //var host = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseConfiguration(config)
-            //    .UseIISIntegration()
-            //    .UseStartup<Startup>()
-            //    .Build();
-            
-            //host.Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
 
             //This line below is use in part one of the tutotial
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
