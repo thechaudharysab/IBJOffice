da3c439a48ff70db7bad3a9f9b99c92538395d7c
Chaudhry Talha <gameral@live.com>
2020-06-23 16:24:30 +0800

    setup for heroku

--- Program.cs
+++ Program.cs
@@ -15,9 +15,16 @@ namespace IBJOffice
     {
         public static void Main(string[] args)
         {
-        //This setting below isused by part 2 of the tutorial
+            //This setting below isused by part 2 of the tutorial
             var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            var host = new WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory()).UseConfiguration(config).UseIISIntegration().UseStartup<Startup>().Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
 
             host.Run();
 
